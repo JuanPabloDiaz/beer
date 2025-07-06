0e68693e434a2d10d83104597ac0f30af9965823
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-05 22:19:40 -0400

    chore: update Inter font format from woff2 to ttf for better browser compatibility

--- plugins/font-loader.client.ts
+++ plugins/font-loader.client.ts
@@ -4,12 +4,11 @@ export default defineNuxtPlugin(() => {
   if (process.client) {
     // Create a link preload element for the Inter font
     const fontPreload = document.createElement("link");
-    fontPreload.rel = "preload";
-    fontPreload.as = "font";
-    fontPreload.type = "font/woff2";
-    fontPreload.crossOrigin = "anonymous";
-    fontPreload.href =
-      "https://fonts.gstatic.com/s/inter/v19/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2";
+    fontPreload.rel = 'preload';
+    fontPreload.as = 'font';
+    fontPreload.type = 'font/ttf';
+    fontPreload.crossOrigin = 'anonymous';
+    fontPreload.href = 'https://fonts.gstatic.com/s/inter/v19/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfMZg.ttf';
     document.head.appendChild(fontPreload);
 
     // Add font-display: swap to any Google Fonts stylesheet
